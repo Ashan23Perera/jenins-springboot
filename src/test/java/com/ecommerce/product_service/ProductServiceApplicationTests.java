1a39c18fd29f9bc5e7438aa28a05ead8ade7b0b9
AshanPerera <ashan@bcs.sg>
2025-12-11 12:46:27 +0800

    junits for product api

--- src/test/java/com/ecommerce/product_service/ProductServiceApplicationTests.java
+++ src/test/java/com/ecommerce/product_service/ProductServiceApplicationTests.java
@@ -1,13 +1,28 @@
 package com.ecommerce.product_service;
 
+import com.ecommerce.product_service.controller.ProductController;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ProductController.class)
 class ProductServiceApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	public void testHelloEndpoint() throws Exception {
+		String expectedJson = "[\"Product1\",\"Product2\",\"Product3\"]";
+
+		mockMvc.perform(get("/api/product"))
+				.andExpect(status().isOk())
+				.andExpect(content().json(expectedJson));
 	}
 
 }
